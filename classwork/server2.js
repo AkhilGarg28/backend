78054331e691ccffd669a863127ac35643ee9211
Akhil Garg <akhilgarg064@gmail.com>
2026-02-16 11:33:31 +0530

    getting user name and age

--- classwork/server2.js
+++ classwork/server2.js
@@ -1,4 +1,8 @@
 const http = require('http');
+const userData = {
+    name: "Akhil",
+    age: 20
+};
 const server = http.createServer((req, res) => {
     const url = req.url;
     const method = req.method;
@@ -6,9 +10,14 @@ const server = http.createServer((req, res) => {
         res.end('Hello, World!');
     } else if (url === '/about' && method === 'GET') {
         res.end('This is the about page.');
+    }else if (url === '/user' && method === 'GET') {
+        res.writeHead(200, { 'Content-Type': 'application/json' });
+        res.end(JSON.stringify(userData));
     }
     console.log(url, method);
 });
     server.listen(4000, () => {
     console.log('Server is running on http://localhost:4000');
+    console.log('Try visiting: http://localhost:4000/user');
+    console.log('Try visiting: http://localhost:4000/about');
 });
